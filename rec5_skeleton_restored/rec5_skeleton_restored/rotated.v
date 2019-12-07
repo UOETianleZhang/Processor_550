32bcc62ad316b67eb46f4f499fa0e934ced7d730
Tianle Zhang <tianle_zhang@outlook.com>
2019-12-07 10:42:20 -0500

    change

--- rec5_skeleton_restored/rec5_skeleton_restored/rotated.v
+++ rec5_skeleton_restored/rec5_skeleton_restored/rotated.v
@@ -11,17 +11,17 @@ module rotated(
 	assign rx = centralPoint / 12;
 	assign ry = centralPoint % 12;
 
-  wire [143:0] signals;
-  genvar i;
-  generate 
-    for (i = 0; i < 144; i = i + 1) begin: for2
-		
-		if (i + 12 >= 144) 
-			assign signals[i] = currentSqs[i];
-		else
-			assign signals[i] = currentSqs[i] && backGround[i + 12];
-    end
-  endgenerate
-  
+//  wire [143:0] signals;
+//  genvar i;
+//  generate 
+//    for (i = 0; i < 144; i = i + 1) begin: for2
+//		
+//		if (i + 12 >= 144) 
+//			assign signals[i] = currentSqs[i];
+//		else
+//			assign signals[i] = currentSqs[i] && backGround[i + 12];
+//    end
+//  endgenerate
+//  
 
 endmodule
